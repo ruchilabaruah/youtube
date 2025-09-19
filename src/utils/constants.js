e29c5ecc1cd16b9a9a50ea9c7154ac118f2e9d12
Ruchila Baruah <ruchilabaruah92@gmail.com>
2025-09-19 09:53:32 +0200

    protect api keys by moving to env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const YT_API_KEY = "AIzaSyCuc6QoZoBUAp5UoxX_ZVLHEmRHKkgnKXk";
+const YT_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YT_POPULAR_VIDEOS_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
